08ea8605cde33e82391bb7619eec86e7517a4918
aaronmwhitehead <aaronwhitehead4@gmail.com>
2022-04-11 14:49:18 -0400

    fix app.js

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use('/', emails);
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // app.get('/:id', (req, res) => emails.getExisting(req, res));
 // app.post('/:id', (req, res) => emails.updateEmail(req, res))
 // app.post('/', (req, res) => emails.saveEmail(req, res))
 
-// app.get("*", function (request, response) {
-//   console.log('get')
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
+app.get("*", function (request, response) {
+  console.log('get')
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // use Routes
 // app.use('/api/emails', emails);
