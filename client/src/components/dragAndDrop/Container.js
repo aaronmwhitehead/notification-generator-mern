0fcde0b3d82078a0f5cd49118870eca5f7a9c813
aaronmwhitehead <aaronwhitehead4@gmail.com>
2022-04-27 11:06:36 -0400

    Updated spacing on Container

--- client/src/components/dragAndDrop/Container.js
+++ client/src/components/dragAndDrop/Container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
     border: 1px solid #ddd;
     background: #f2f4f6;
-    padding: 8em 1em;
+    padding: 9em 1em;
     flex: 0 0 150px;
     font-family: sans-serif;
     height: 100vh;
